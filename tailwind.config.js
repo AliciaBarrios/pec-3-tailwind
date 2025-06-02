e77d7463a98fc34460596f536e3f72773b5fbeb8
Alicia Barrios Garcia <abarriosga@uoc.edu>
2025-06-02 18:47:01 +0200

    first commit: add all files

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,scss}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'var(--color-primary)',
+        secondary: 'var(--color-secondary)',
+        bg: 'var(--color-bg)',
+      }
+    },
+  },
+  plugins: [],
+}
+
